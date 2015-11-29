9a5a73e69adbdf408ca86c489d210420ac341b57
Chittaranjan Swaminathan Srinivas <chittaranjan.swaminathan@oru.se>
2015-11-29 22:57:10 +0100

    added license

--- src/manipulation_services_node.cpp
+++ src/manipulation_services_node.cpp
@@ -1,10 +1,20 @@
 /*
- * manipulation_services_node.cpp
- *
- *  Created on: Oct 3, 2014
- *      Author: ace
- */
+  Copyright (C) 2015  Chittaranjan Srinivas Swaminathan
 
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>
+  
+*/
 #include <ros/ros.h>
 #include <doro_manipulation/grasp_pose_generator.h>
 #include <doro_manipulation/doro_manipulation.h>
--- src/put_down_points_generator.cpp
+++ src/put_down_points_generator.cpp
@@ -1,9 +1,20 @@
 /*
- * put_down_points_generator.cpp
- *
- *  Created on: Oct 3, 2014
- *      Author: ace
- */
+  Copyright (C) 2015  Chittaranjan Srinivas Swaminathan
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>
+  
+*/
 
 #include <doro_manipulation/put_down_points_generator.h>
 
