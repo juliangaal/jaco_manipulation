0d9d6a0e31ce4536c2a8e02a2f596453922c5d8e
juliangaal <juliangaal@protonmail.com>
2018-08-02 15:16:30 +0200

    prep for setting boundaries to workspace

--- src/jaco_boundaries.cpp
+++ src/jaco_boundaries.cpp
@@ -2,4 +2,8 @@
 // Created by chitt on 2018-08-02.
 //
 
-#include "jaco_boundaries.hpp"
+#include "jaco_manipulation/jaco_boundaries.h"
+#include <ros/console.h>
+
+JacoBoundaries::JacoBoundaries() {
+}
