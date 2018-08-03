5a6c3db6d79eeb018efdade2f3a10a941d69a0e3
juliangaal <juliangaal@protonmail.com>
2018-08-03 10:45:07 +0200

    give new target pose: doro home pose

--- src/jaco_manipulation_client_test.cpp
+++ src/jaco_manipulation_client_test.cpp
@@ -10,13 +10,13 @@ int main(int argn, char* args[]) {
   jaco_manipulation::PlanAndMoveArmGoal goal;
   goal.goal_type = "pose";
   goal.target_pose.header.frame_id = "root";
-  goal.target_pose.pose.position.x = -0.038;
-  goal.target_pose.pose.position.y = -0.223;
-  goal.target_pose.pose.position.z = 0.505;
-  goal.target_pose.pose.orientation.x = -0.3172;
-  goal.target_pose.pose.orientation.y =  0.6319;
-  goal.target_pose.pose.orientation.z = 0.6319;
-  goal.target_pose.pose.orientation.w = -0.3172;
+  goal.target_pose.pose.position.x = 0.063846;
+  goal.target_pose.pose.position.y = -0.193645;
+  goal.target_pose.pose.position.z = 0.509365;
+  goal.target_pose.pose.orientation.x = 0.369761;
+  goal.target_pose.pose.orientation.y =  -0.555344;
+  goal.target_pose.pose.orientation.z = -0.661933;
+  goal.target_pose.pose.orientation.w = 0.341635;
 
   pam_client.waitForServer();
   ROS_INFO("Calling jaco_manipulation...");
