99cda79105bca6e14200b7e0008367a4b054af73
juliangaal <juliangaal@protonmail.com>
2018-08-03 16:16:02 +0200

    restructure test, add multiple scenarios

--- src/jaco_manipulation_client_test.cpp
+++ src/jaco_manipulation_client_test.cpp
@@ -47,9 +47,9 @@ class Executer {
     ROS_INFO_STREAM("Attempt: " << move.description);
 
     if (move.start.goal.goal_type.empty() && moveArm(move.end)) // start goal emoty: start at current state
-      ROS_SUCCESS("Success: " + move.description);
+      ROS_SUCCESS("Success : " + move.description);
     else if (moveArm(move.start) && moveArm(move.end)) // start and goal state occupied
-      ROS_SUCCESS("Success: " + move.description);
+      ROS_SUCCESS("Success : " + move.description);
     else // if one of start or goal move fails
       ROS_ERROR_STREAM("Fail   : " << move.description);
   }
@@ -61,10 +61,13 @@ class Executer {
     client_.sendGoal(goal.goal);
     client_.waitForResult();
 
-    if (client_.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-      ROS_SUCCESS("Move to " + goal.description + " succeeded.");
-    else
-      ROS_ERROR_STREAM("Move to " << goal.description << " failed.");
+    if (client_.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
+      ROS_SUCCESS("Status : Move to " + goal.description + " succeeded.");
+      return true;
+    } else {
+      ROS_ERROR_STREAM("Status : Move to " << goal.description << " failed.");
+      return false;
+    }
   }
 
   vector<Move> moves;
@@ -83,9 +86,11 @@ int main(int argn, char *args[]) {
     move.description = "Joint target: Initial pos -> home joint state";
 
     jaco_manipulation::PlanAndMoveArmGoal end_goal;
-    end_goal.goal_type = "joint_state_1";
+    end_goal.goal_type = "home";
     move.end.goal = end_goal;
     move.end.description = "home joint state";
+
+    assert(!move.end.goal.goal_type.empty());
 
     exe.addMove(move);
   }
@@ -104,7 +109,86 @@ int main(int argn, char *args[]) {
     move.end.goal = end_goal;
     move.end.description = "joint state 1";
 
-    //exe.addMove(move);
+    assert(!move.start.goal.goal_type.empty());
+    assert(!move.end.goal.goal_type.empty());
+
+    exe.addMove(move);
+  }
+
+  {
+    Move move;
+    move.description = "Joint target: joint state 1 -> joint state 2";
+
+    jaco_manipulation::PlanAndMoveArmGoal start_goal;
+    start_goal.goal_type = "joint_state_1";
+    move.start.goal = start_goal;
+    move.start.description = "joint state 1";
+
+    jaco_manipulation::PlanAndMoveArmGoal end_goal;
+    end_goal.goal_type = "joint_state_2";
+    move.end.goal = end_goal;
+    move.end.description = "joint state 2";
+
+    assert(!move.start.goal.goal_type.empty());
+    assert(!move.end.goal.goal_type.empty());
+
+    exe.addMove(move);
+  }
+
+  {
+    Move move;
+    move.description = "Joint target -> Pose target: joint state 2 -> home pose";
+
+    jaco_manipulation::PlanAndMoveArmGoal start_goal;
+    start_goal.goal_type = "joint_state_2";
+    move.start.goal = start_goal;
+    move.start.description = "joint state 2";
+
+    jaco_manipulation::PlanAndMoveArmGoal end_goal;
+    end_goal.goal_type = "pose";
+    end_goal.target_pose.header.frame_id = "root";
+    end_goal.target_pose.pose.position.x = 0.063846;
+    end_goal.target_pose.pose.position.y = -0.193645;
+    end_goal.target_pose.pose.position.z = 0.509365;
+    end_goal.target_pose.pose.orientation.x = 0.369761;
+    end_goal.target_pose.pose.orientation.y =  -0.555344;
+    end_goal.target_pose.pose.orientation.z = -0.661933;
+    end_goal.target_pose.pose.orientation.w = 0.341635;
+    move.end.goal = end_goal;
+    move.end.description = "home pose";
+
+    assert(!move.start.goal.goal_type.empty());
+    assert(!move.end.goal.goal_type.empty());
+
+    exe.addMove(move);
+  }
+
+  {
+    Move move;
+    move.description = "Pose target -> Joint target: home pose -> home joint state";
+
+    jaco_manipulation::PlanAndMoveArmGoal start_goal;
+    start_goal.goal_type = "pose";
+    start_goal.target_pose.header.frame_id = "root";
+    start_goal.target_pose.pose.position.x = 0.063846;
+    start_goal.target_pose.pose.position.y = -0.193645;
+    start_goal.target_pose.pose.position.z = 0.509365;
+    start_goal.target_pose.pose.orientation.x = 0.369761;
+    start_goal.target_pose.pose.orientation.y =  -0.555344;
+    start_goal.target_pose.pose.orientation.z = -0.661933;
+    start_goal.target_pose.pose.orientation.w = 0.341635;
+    move.start.goal = start_goal;
+    move.start.description = "home pose";
+
+    jaco_manipulation::PlanAndMoveArmGoal end_goal;
+    end_goal.goal_type = "home";
+    move.end.goal = end_goal;
+    move.end.description = "home joint state";
+
+    assert(!move.start.goal.goal_type.empty());
+    assert(!move.end.goal.goal_type.empty());
+
+    exe.addMove(move);
   }
 
   exe.execute();
