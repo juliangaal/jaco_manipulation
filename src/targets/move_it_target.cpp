f13ef1732dfed18f61340337952cee3d2860f8fb
Julian Gaal <gjulian@uos.de>
2018-08-05 01:39:14 +0200

    rename for consistency

--- src/targets/move_it_target.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by julian on 04.08.18.
-//
-
-#include "../../include/jaco_manipulation/targets/move_it_target.h"
--- src/targets/pose_target.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by julian on 04.08.18.
-//
-
-#include "../../include/jaco_manipulation/targets/pose_target.h"
