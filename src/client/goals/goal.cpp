c968249945a3cafb2f5ee752627bced3ef30c24f
juliangaal <juliangaal@protonmail.com>
2018-08-05 12:57:53 +0200

    integrate goal base class

---
+++ src/client/goals/goal.cpp
@@ -0,0 +1,17 @@
+//
+// Created by julian on 04.08.18.
+//
+
+#include <jaco_manipulation/client/goals/move_it_goal.h>
+
+MoveItGoal::MoveItGoal() : planning_frame("root") {
+  goal.goal_type = "home";
+}
+
+MoveItGoal::MoveItGoal(const string &name) : planning_frame("root") {
+  goal.goal_type = name;
+}
+
+jaco_manipulation::PlanAndMoveArmGoal MoveItGoal::getGoal() const {
+  return goal;
+}
