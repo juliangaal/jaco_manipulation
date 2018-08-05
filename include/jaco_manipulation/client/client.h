5d9a7e611b2f51d9a80518d99f75c622e4c8541f
Julian Gaal <gjulian@uos.de>
2018-08-05 03:18:41 +0200

    integrate client and goals

---
+++ include/jaco_manipulation/client/client.h
@@ -0,0 +1,34 @@
+//
+// Created by julian on 05.08.18.
+//
+
+#ifndef PROJECT_CLIENT_H
+#define PROJECT_CLIENT_H
+
+#include <actionlib/client/simple_action_client.h>
+#include <jaco_manipulation/PlanAndMoveArmAction.h>
+#include <jaco_manipulation/server/jaco_manipulation.h>
+
+#include "joint_goal.h"
+#include "move_it_goal.h"
+#include "pose_goal.h"
+
+using PamClient = actionlib::SimpleActionClient<jaco_manipulation::PlanAndMoveArmAction>;
+
+namespace Moveit {
+
+    class Client {
+    public:
+        explicit Client();
+
+        ~Client();
+
+        template <typename T>
+        void execute(const T &goal);
+    private:
+        PamClient client_;
+    };
+}
+
+
+#endif //PROJECT_CLIENT_H
