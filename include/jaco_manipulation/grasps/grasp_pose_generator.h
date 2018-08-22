a4df8b9e47ffa42046d729da2e44a82d0cfc80ee
juliangaal <juliangaal@protonmail.com>
2018-08-22 13:08:01 +0200

    comments

--- include/jaco_manipulation/grasps/grasp_pose_generator.h
+++ include/jaco_manipulation/grasps/grasp_pose_generator.h
@@ -14,37 +14,128 @@
 namespace jaco_manipulation {
 namespace grasps {
 
+/// enum to define all grasp types
 enum GraspType { TOP_GRASP, TOP_DROP, FRONT_GRASP, LEFT_GRASP, RIGHT_GRASP };
 
+/**
+ * GraspPoseGenerator generates poses according to specified pose in GraspType enum
+ */
 class GraspPoseGenerator {
  public:
+  /**
+   * Constructor
+   */
   GraspPoseGenerator();
+
+  /**
+   * default destructor
+   */
+  ~GraspPoseGenerator() = default;
+
+  /**
+   * Adjusts Pose
+   * @param pose input pose
+   * @param box bounding box to be adjusted to
+   * @param type grasp type yo adjust to
+   */
   void adjustPose(geometry_msgs::PoseStamped &pose,
                   const jaco_manipulation::BoundingBox &box,
                   const GraspType type);
+
+  /**
+ * Adjusts Pose
+ * @param pose input pose
+ * @param type grasp type yo adjust to
+ */
+  void adjustPose(geometry_msgs::PoseStamped &pose,
+                  const GraspType type);
+
  private:
+  /**
+   * Adjusts height for top pose
+   * @param pose input pose
+   * @param box input bounding box
+   */
   void adjustHeightForTopPose(geometry_msgs::PoseStamped &pose,
-                                     const jaco_manipulation::BoundingBox &box);
+                              const jaco_manipulation::BoundingBox &box);
+
+  /**
+   * Adjusts height for top pose
+   * @param pose input pose
+   * @param box input bounding box
+   */
   void adjustHeightForTopDropPose(geometry_msgs::PoseStamped &pose,
-                                     const jaco_manipulation::BoundingBox &box);
+                                  const jaco_manipulation::BoundingBox &box);
+
+  /**
+   * Adjusts height for front pose
+   * @param pose input pose
+   * @param box input bounding box
+   */
   void adjustHeightForFrontPose(geometry_msgs::PoseStamped &pose,
-                                                    const jaco_manipulation::BoundingBox &box);
+                                const jaco_manipulation::BoundingBox &box);
+
+  /**
+   * Adjust position from bounding box centroid
+   * @param pose input pose
+   * @param box input bounding box
+   * @param type grasp type
+   */
   void adjustPosition(geometry_msgs::PoseStamped &pose,
                       const jaco_manipulation::BoundingBox &box,
                       const GraspType type);
-  void adjustToTopOrientation(geometry_msgs::PoseStamped &pose);
-  void adjustToFrontOrientation(geometry_msgs::PoseStamped &pose);
-  void adjustPoseForFrontPose(geometry_msgs::PoseStamped &pose);
 
+  /**
+   * Adjusts orientation for top pose
+   * @param pose input pose
+   */
+  void adjustToTopOrientation(geometry_msgs::PoseStamped &pose);
+
+  /**
+  * Adjusts orientation for front pose
+  * @param pose input pose
+  */
+  void adjustToFrontOrientation(geometry_msgs::PoseStamped &pose);
+
+  /**
+  * Adjusts position for front pose: offset for gripping
+  * @param pose input pose
+  */
+  void adjustPositionForFrontPose(geometry_msgs::PoseStamped &pose);
+
+  /**
+   * transforms goal into robot frame
+   * @param pose input pose
+   * @param box input bounding box
+   */
   void transformGoalIntoRobotFrame(geometry_msgs::PoseStamped &pose,
                                    const jaco_manipulation::BoundingBox &box);
 
+  /**
+ * transforms goal into robot frame
+ * @param pose input pose
+ */
+  void transformGoalIntoRobotFrame(geometry_msgs::PoseStamped &pose);
+
+  /// Node Handle
   ros::NodeHandle n_;
+
+  /// transform listener
   tf::TransformListener tf_listener_;
+
+  /// Minimum height for top grasp
   constexpr static double min_height_top_grasp = 0.175026;
+
+  /// Minimum height for front grasp
   constexpr static double min_height_front_grasp = 0.011;
+
+  /// offset that is added for dropping
   constexpr static double drop_offset_ = 0.03;
+
+  /// offset to stack two objects
   constexpr static double stack_offset_ = 0.01;
+
+  /// offset for grasping: distance between jaco_link_hand and palm
   constexpr static double grasp_offset_ = 0.145;
 };
 
