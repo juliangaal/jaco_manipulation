71d9e5a009eaa65d76fb13e64e840f992d3010ba
juliangaal <juliangaal@protonmail.com>
2018-09-28 14:49:30 +0200

    add gitignore

--- scripts/test.py
+++ scripts/test.py
@@ -1,20 +1,15 @@
-#!/usr/bin/env python
-# kinetic only support python 2.7
-
-import rospy
 import os
-from jaco_manipulation.msg import JacoDebug
-
 
 class Test:
-    def __init__(self, filename):
+    def __init__(self, filename, labels='empty labels'):
         self.dir_path = os.path.dirname(os.path.realpath(__file__))
         self.filename = self.dir_path + '/' + filename
+        self.labels = labels
         self.__setup_log_file()
 
     def __setup_log_file(self):
         self.file = open(self.filename, "w")
-        self.write("Time,CurrentPose,TargetPose,Result")
+        self.write(self.labels)
 
     def write(self, line):
         self.file.write(line + '\n')
@@ -29,29 +24,3 @@ class Test:
     def __del__(self):
         self.file.close()
         print "Logged to", self.filename
-
-
-class BaseLineTest(Test):
-    def __init__(self, filename, delimiter=','):
-        Test.__init__(self, filename)
-        self.delimiter = delimiter
-        rospy.init_node('base_line_tester', anonymous=True)
-        rospy.Subscriber("jaco_manipulation/debug", JacoDebug, self.__callback)
-        rospy.spin()
-
-    def __msg_to_string(self, msg):
-        time = msg.timestamp
-        current_pose = Test.pose_to_string(self, msg.goal.current_pose)
-        target_pose  = Test.pose_to_string(self, msg.goal.target_pose)
-        result = 'success' if msg.result == 'True' else 'failure'
-        return time + self.delimiter + current_pose + self.delimiter + target_pose + self.delimiter + result
-
-    def __callback(self, msg):
-        line = self.__msg_to_string(msg)
-        self.__write(line)
-
-    def __write(self, line):
-        Test.write(self, line)
-
-
-test = BaseLineTest("baseline.csv")
