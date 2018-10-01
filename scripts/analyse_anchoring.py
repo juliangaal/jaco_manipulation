a7b3af8b53c022250113287fcdd0cae0b4cf9d06
juliangaal <juliangaal@protonmail.com>
2018-10-01 16:18:33 +0200

    adjust for new message format at parameter "result"

--- scripts/analyse_anchoring.py
+++ scripts/analyse_anchoring.py
@@ -1,4 +1,4 @@
 from analyse import ResultPlotter
 
-plotter = ResultPlotter('results/baseline/combined/baseline_combined_recording.csv', ['Time', 'CurrentPose', 'TargetPose', 'Result'], ';')
+plotter = ResultPlotter('anchoring_test_recording.csv', ['Time', 'Command', 'Type', 'CurrentPose', 'TargetPose', 'Result'], ';')
 plotter.saveResultFrom('TargetPose')
