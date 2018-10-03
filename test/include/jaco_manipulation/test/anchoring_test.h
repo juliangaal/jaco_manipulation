c9b7409629c7675aff7b259707f46d4aeec7e156
juliangaal <juliangaal@protonmail.com>
2018-10-03 16:36:55 +0200

    refactor anchoring test class for future inheritance

--- test/include/jaco_manipulation/test/anchoring_test.h
+++ test/include/jaco_manipulation/test/anchoring_test.h
@@ -15,4 +15,48 @@
 #ifndef PROJECT_ANCHORING_TEST_H
 #define PROJECT_ANCHORING_TEST_H
 
+#include <jaco_manipulation/client/jaco_manipulation_client.h>
+#include <jaco_manipulation/BoundingBox.h>
+#include <anchor_msgs/AnchorArray.h>
+#include <jaco_manipulation/units.h>
+
+namespace jaco_manipulation {
+namespace test {
+
+class AnchorTest {
+ public:
+  AnchorTest() = delete;
+
+  AnchorTest(const std::vector<BoundingBox> &datapoints);
+
+  ~AnchorTest() = default;
+
+  void anchorArrayCallback(const anchor_msgs::AnchorArray::ConstPtr &msg);
+
+  bool anchors_published() const;
+
+ private:
+  const std::vector<BoundingBox> &data;
+  size_t trial_counter;
+  size_t grip_counter;
+  bool found_anchor;
+  const std::string topic;
+  BoundingBox drop_box;
+  ros::NodeHandle n;
+  ros::Subscriber sub;
+  anchor_msgs::AnchorArray anchors;
+  client::JacoManipulationClient jmc;
+  std::vector<BoundingBox>::const_iterator current_box_it;
+
+  std::vector<jaco_manipulation::BoundingBox>::const_iterator next_point();
+
+  jaco_manipulation::BoundingBox createBoundingBoxFromAnchors() const;
+
+  void show_summary(const std::vector<std::string> &labels) const;
+
+  void show_test_info();
+};
+} // namespace tes
+} // namespace jaco_manipulation
+
 #endif //PROJECT_ANCHORING_TEST_H
