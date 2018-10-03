19b1e3904ed5a1739eec014eddb14a4b78bb6a73
juliangaal <juliangaal@protonmail.com>
2018-10-03 11:40:03 +0200

    finish anchoring test with ~200 data points

--- scripts/analyse_anchoring.py
+++ scripts/analyse_anchoring.py
@@ -1,5 +1,5 @@
 from analyse import ResultPlotter
 
-plotter = ResultPlotter('results/anchoring/01/01_anchoring_test_recording.csv', ['Time', 'Type', 'CurrentPose', 'TargetPose', 'Result', 'Gripped'], ';')
+plotter = ResultPlotter('results/anchoring/combined/anchoring_test_combined_recording.csv', ['Time', 'Type', 'CurrentPose', 'TargetPose', 'Result', 'Gripped'], ';')
 plotter.save3DResultFrom('TargetPose','Gripped')
 plotter.save2DResultFrom('TargetPose','Gripped')
