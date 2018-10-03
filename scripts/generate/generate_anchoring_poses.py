04f417bed1ba84449ab46b7687ee822399229045
juliangaal <juliangaal@protonmail.com>
2018-10-03 16:36:19 +0200

    refactor pose generation

--- scripts/generate/generate_anchoring_poses.py
+++ scripts/generate/generate_anchoring_poses.py
@@ -1,4 +1,6 @@
 from pose_generator import PoseGenerator
 
 # generated 50 drop poses for anchoring test
-g = PoseGenerator('anchoring_poses.csv', total_poses=100)
+if __name__ == "__main__":
+    g = PoseGenerator('anchoring_poses.csv', total_poses=100)
+    g.generate()
--- scripts/generate/pose_generator.py
+++ scripts/generate/pose_generator.py
@@ -5,18 +5,17 @@ import random
 
 class PoseGenerator:
     def __init__(self, file_path, total_poses):
-        self.dir_path = os.path.dirname(os.path.realpath(__file__))
-        self.file_path = self.dir_path + '/' + file_path
+        self.parent_dir = os.path.dirname(os.getcwd())
+        self.file_path = self.parent_dir + '/' + file_path
         self.file = open(self.file_path, "w")
         self.file.write("x,y,z,dim_x,dim_y,dim_z\n")
         self.total_poses = total_poses;
-        self.__generate()
 
     def __del__(self):
         self.file.close()
         print "generated", self.total_poses, "datapoints in", self.file_path
 
-    def __generate(self):
+    def generate(self):
         num_of_pose = 0
         while num_of_pose < self.total_poses:
             x = random.uniform(0.25, 0.7)
